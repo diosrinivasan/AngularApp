55a6aae547ac17eb0766aeb8dcc0a4451f5b271d
srini23121987 <ursfriendly.sri23@gmail.com>
2017-11-25 00:14:34 +0530

    new commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,26 +1,28 @@
 $(function () {
-    $('.panel-google-plus > .panel-footer > .input-placeholder, .panel-google-plus > .panel-google-plus-comment > .panel-google-plus-textarea > button[type="reset"]').on('click', function(event) {
-         var $panel = $(this).closest('.panel-google-plus');
-             $comment = $panel.find('.panel-google-plus-comment');
-             
-         $comment.find('.btn:first-child').addClass('disabled');
-         $comment.find('textarea').val('');
-         
-         $panel.toggleClass('panel-google-plus-show-comment');
-         
-         if ($panel.hasClass('panel-google-plus-show-comment')) {
-             $comment.find('textarea').focus();
-         }
+
+  
+    $('.panel-google-plus > .panel-footer > .input-placeholder, .panel-google-plus > .panel-google-plus-comment > .panel-google-plus-textarea > button[type="reset"]').on('click', function (event) {
+        var $panel = $(this).closest('.panel-google-plus');
+        $comment = $panel.find('.panel-google-plus-comment');
+
+        $comment.find('.btn:first-child').addClass('disabled');
+        $comment.find('textarea').val('');
+
+        $panel.toggleClass('panel-google-plus-show-comment');
+
+        if ($panel.hasClass('panel-google-plus-show-comment')) {
+            $comment.find('textarea').focus();
+        }
     });
-    $('.panel-google-plus-comment > .panel-google-plus-textarea > textarea').on('keyup', function(event) {
-         var $comment = $(this).closest('.panel-google-plus-comment');
-         
-         $comment.find('button[type="submit"]').addClass('disabled');
-         if ($(this).val().length >= 1) {
-             $comment.find('button[type="submit"]').removeClass('disabled');
-         }
+    $('.panel-google-plus-comment > .panel-google-plus-textarea > textarea').on('keyup', function (event) {
+        var $comment = $(this).closest('.panel-google-plus-comment');
+
+        $comment.find('button[type="submit"]').addClass('disabled');
+        if ($(this).val().length >= 1) {
+            $comment.find('button[type="submit"]').removeClass('disabled');
+        }
     });
- });
+});
 var app = angular.module('myapp', ['ngAnimate', 'ui.bootstrap']);
 app.factory("States", function () {
     var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Dakota", "North Carolina", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];

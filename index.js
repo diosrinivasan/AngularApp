522013c0d90772937df6619f4d94c453edc2c25d
srini23121987 <ursfriendly.sri23@gmail.com>
2017-11-24 06:41:55 +0530

    new commi

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var express = require('express'),
 app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.json());
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
+});
+app.get('/login', function (req, res) {
+    res.sendFile(__dirname + '/public/login.html');
 });
 app.listen(process.env.PORT||7000);

197cd69efbbbad6ed789fd772a4c896a3fe127f4
srini23121987 <ursfriendly.sri23@gmail.com>
2017-11-25 12:42:17 +0530

    new commit

--- public/js/script.js
+++ public/js/script.js
@@ -23,43 +23,43 @@ $(function () {
         }
     });
 });
-var app = angular.module('myapp', ['ngAnimate', 'ui.bootstrap']);
-app.factory("States", function () {
-    var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Dakota", "North Carolina", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
-    return states;
-});
-app.controller('RatingDemoCtrl', function ($scope) {
-});
-app.controller("TypeaheadCtrlS", function ($scope, States) {
+// var app = angular.module('myapp', ['ngAnimate', 'ui.bootstrap']);
+// app.factory("States", function () {
+//     var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Dakota", "North Carolina", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
+//     return states;
+// });
+// app.controller('RatingDemoCtrl', function ($scope) {
+// });
+// app.controller("TypeaheadCtrlS", function ($scope, States) {
 
-    $scope.sourceS = undefined;
+//     $scope.sourceS = undefined;
 
-    $scope.source = States;
+//     $scope.source = States;
 
-});
-app.controller("TypeaheadCtrlD", function ($scope, States) {
+// });
+// app.controller("TypeaheadCtrlD", function ($scope, States) {
 
-    $scope.destinationD = undefined;
+//     $scope.destinationD = undefined;
 
-    $scope.destination = States;
+//     $scope.destination = States;
 
-});
-app.controller("DateController", function ($scope, States) {
-    $scope.popup1 = {
-        opened: false
-    };
-    $scope.today = function () {
-        $scope.date = new Date().getDate() + "-" + new Date().getMonth() + "-" + new Date().getFullYear();
-    };
-    $scope.today();
-    $scope.open1 = function () {
-        $scope.popup1.opened = true;
-    };
-    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-    $scope.format = $scope.formats[0];
-    $scope.dateOptions = {
-        'year-format': "'yy'",
-        'starting-day': 1,
-        'minDate': new Date().getDate() + "-" + new Date().getMonth() + "-" + new Date().getFullYear()
-    };
-});
+// });
+// app.controller("DateController", function ($scope, States) {
+//     $scope.popup1 = {
+//         opened: false
+//     };
+//     $scope.today = function () {
+//         $scope.date = new Date().getDate() + "-" + new Date().getMonth() + "-" + new Date().getFullYear();
+//     };
+//     $scope.today();
+//     $scope.open1 = function () {
+//         $scope.popup1.opened = true;
+//     };
+//     $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+//     $scope.format = $scope.formats[0];
+//     $scope.dateOptions = {
+//         'year-format': "'yy'",
+//         'starting-day': 1,
+//         'minDate': new Date().getDate() + "-" + new Date().getMonth() + "-" + new Date().getFullYear()
+//     };
+// });
